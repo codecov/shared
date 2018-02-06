0d7ecda169ea5ba90785fd865dc76320f5bd91eb
Thomas Pedbereznak <tom@tomped.com>
2018-02-06 17:17:57 -0500

    tests for color and ratio

--- tests/unit/helpers/test_ratio.py
+++ tests/unit/helpers/test_ratio.py
@@ -1,6 +1,14 @@
 from src.helpers.ratio import ratio
 
 
-def test_ratio():
-    r = ratio(2, 2)
-    assert r == '100'
+def test_ratio_one():
+    assert ratio(2, 2) == '100'
+
+
+def test_ratio_zero():
+    assert ratio(0, 2) == '0'
+    assert ratio(2, 0) == '0'
+
+
+def test_ratio_fraction():
+    assert ratio(1, 2) == '50.00000'
