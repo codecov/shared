652a3141dc6e502c3095518de18f21a721dfdfc2
Thomas Pedbereznak <tom@tomped.com>
2018-03-01 15:54:37 -0500

    ratio tests

---
+++ tests/unit/helpers/test_ratio.py
@@ -0,0 +1,12 @@
+import pytest
+from src.helpers.ratio import ratio
+
+
+@pytest.mark.parametrize('x, y, percent', [
+    (2, 2, '100'),
+    (0, 2, '0'),
+    (2, 0, '0'),
+    (1, 2, '50.00000'),
+])
+def test_ratio(x, y, percent):
+    assert ratio(x, y) == percent
