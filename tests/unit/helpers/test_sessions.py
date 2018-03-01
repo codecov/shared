bf6913b7cd750678a481aa7932ed23c126b86a42
Thomas Pedbereznak <tom@tomped.com>
2018-03-01 15:55:32 -0500

    sessions tests

---
+++ tests/unit/helpers/test_sessions.py
@@ -0,0 +1,18 @@
+from src.helpers.sessions import Session
+
+
+def test_sessions():
+    s = Session('id', 'totals', 'time', 'archive', 'flags', 'provider', 'build', 'job', 'url', 'state', 'env', 'name')
+    assert s._encode() == {
+            't': 'totals',
+            'd': 'time',
+            'a': 'archive',
+            'f': 'flags',
+            'c': 'provider',
+            'n': 'build',
+            'N': 'name',
+            'j': 'job',
+            'u': 'url',
+            'p': 'state',
+            'e': 'env',
+        }
