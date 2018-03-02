d764e48218103f7e1432e1cb4b0e3e1b2de60edd
Thomas Pedbereznak <tom@tomped.com>
2018-03-02 18:59:55 -0500

    sum_totals tests

---
+++ tests/unit/utils/test_sum_totals.py
@@ -0,0 +1,12 @@
+import pytest
+from src.utils.tuples import ReportTotals
+from src.utils.sum_totals import sum_totals
+
+
+@pytest.mark.parametrize('totals, res', [
+    ([ReportTotals(), ReportTotals(3, 3, 3, 3), ReportTotals()], ReportTotals(3, 3, 3, 3, 0, '100')),
+    ([ReportTotals()], ReportTotals(1, 0, 0, 0, 0, '100')),
+    ([], ReportTotals())
+])
+def test_sum_totals(totals, res):
+    assert sum_totals(totals) == res
