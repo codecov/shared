5c8c24a0b6558846999de63bfd071d032225d4c8
Thomas Pedbereznak <tom@tomped.com>
2018-03-02 16:21:34 -0500

    make_network_file tests

---
+++ tests/unit/utils/test_make_network_file.py
@@ -0,0 +1,10 @@
+from src.utils.tuples import ReportTotals, NetworkFile
+from src.utils.make_network_file import make_network_file
+
+
+def test_make_network_file():
+    assert make_network_file([1, 2, 1, 1]) == NetworkFile(
+        totals=ReportTotals(files=1, lines=2, hits=1, misses=1, partials=0, complexity=0, complexity_total=0, diff=0),
+        session_totals=None,
+        diff_totals=None
+    )
