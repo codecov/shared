dc29668f651825cca36376d76c1dd830591a12d2
Thomas Pedbereznak <tom@tomped.com>
2018-03-20 17:54:14 -0400

    tests for assume paths

--- tests/unit/test_report.py
+++ tests/unit/test_report.py
@@ -206,13 +206,23 @@ def test_merge(r, new_report, manifest):
     assert r.manifest == manifest
 
 
-def test_merge_exception():
+def test_assume_flags():
+    prev_r = Report(files={
+        'file.py': [0, ReportTotals(1)]
+    }, chunks=[ReportFile(name='file.py', totals=[1, 2, 1], lines=[ReportLine(1), ReportLine(2)])], sessions={1: {
+        'id': 'id',
+        'f': ['int'],
+    }})
+
     r = Report(files={
-        'file.py': [0, ReportTotals(1)],
-    })
-    with pytest.raises(Exception) as e_info:
-        r.merge('str')
-    assert e_info.value.message == "expecting type Report got <type 'str'>"
+        'py.py': [0, ReportTotals(1)]
+    }, chunks=[ReportFile(name='py.py', totals=[1, 2, 1])])
+
+    assert repr(r) == '<Report files=1>'
+    assert r.sessions.keys() == []
+    r.assume_flags('int', prev_r)
+    assert repr(r) == '<Report files=2>'
+    assert r.sessions.keys() == [0]  # TODO filter should work
 
 
 @pytest.mark.parametrize('r, boolean', [
