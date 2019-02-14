5aeeb8ddafcce2012738f39910645ddc6240cde1
Eli Hooten <elir.hooten@gmail.com>
2019-02-13 20:43:50 -0600

    Fix Error on storage_path
    storage_path in sessions would error if the passed in url did not contain `v4/raw` for splitting.

--- covreports/utils/sessions.py
+++ covreports/utils/sessions.py
@@ -16,7 +16,7 @@ class Session(object):
         self.state = p or kwargs.get('state')
         self.env = e or kwargs.get('env')
         self.name = N or kwargs.get('name')
-        self.storage_path = s or ('v4/raw' + self.archive.split('v4/raw')[1] if self.archive else None)
+        self.storage_path = s or ('v4/raw' + self.archive.split('v4/raw')[1] if self.archive and 'v4/raw' in self.arcchive else None)
 
     def _encode(self):
         return {
