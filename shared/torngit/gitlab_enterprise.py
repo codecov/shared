2bb7b4007779982f76df3b68f2d2fcc6e95b6b9a
Thiago Ramos <thiago@codecov.io>
2020-04-15 11:06:32 -0300

    Fixing Gitlab Enterprise to use proper config
    In legacy, there was some cod that would turn configuration back into envvars (instead of the usual envvars into configuretion) that we didnt port.
    
    It was probably done taht way to deal with the fact that torngit was open source and used by other people, so we couldnt tie to our configurations.
    
    But now we do.

--- shared/torngit/gitlab_enterprise.py
+++ shared/torngit/gitlab_enterprise.py
@@ -2,11 +2,22 @@ import os
 
 from shared.torngit.gitlab import Gitlab
 
+from shared.config import get_config
+
 
 class GitlabEnterprise(Gitlab):
-    service = 'gitlab_enterprise'
-    service_url = os.getenv('GITLAB_ENTERPRISE_URL', '').strip('/')
-    api_url = os.getenv('GITLAB_ENTERPRISE_API_URL', '').strip('/') or (
-        os.getenv('GITLAB_ENTERPRISE_URL', '').strip('/') + '/api/v4')
-    verify_ssl = os.getenv('GITLAB_ENTERPRISE_SSL_PEM') or (
-        os.getenv('GITLAB_ENTERPRISE_VERIFY_SSL') != 'FALSE')
+    service = "gitlab_enterprise"
+
+    @property
+    def service_url(self):
+        return get_config("gitlab_enterprise", "url")
+
+    @property
+    def api_url(self):
+        if get_config("gitlab_enterprise", "api_url"):
+            return get_config("gitlab_enterprise", "api_url")
+        return self.service_url + "/api/v4"
+
+    verify_ssl = os.getenv("GITLAB_ENTERPRISE_SSL_PEM") or (
+        os.getenv("GITLAB_ENTERPRISE_VERIFY_SSL") != "FALSE"
+    )
---
+++ tests/unit/torngit/test_gitlab_enterprise.py
@@ -0,0 +1,56 @@
+from asyncio import Future
+import pytest
+
+from shared.torngit.gitlab_enterprise import GitlabEnterprise
+
+
+class TestGitlabEnterprise(object):
+    def test_urls_no_api_url_set(self, mock_configuration):
+        mock_configuration._params["gitlab_enterprise"] = {
+            "url": "https://gitlab-enterprise.codecov.dev"
+        }
+        gl = GitlabEnterprise()
+        assert gl.service_url == "https://gitlab-enterprise.codecov.dev"
+        assert gl.api_url == "https://gitlab-enterprise.codecov.dev/api/v4"
+
+    def test_urls_with_api_url_set(self, mock_configuration):
+        mock_configuration._params["gitlab_enterprise"] = {
+            "url": "https://gitlab-enterprise.codecov.dev",
+            "api_url": "https://api.gitlab.dev",
+        }
+        gl = GitlabEnterprise()
+        assert gl.service_url == "https://gitlab-enterprise.codecov.dev"
+        assert gl.api_url == "https://api.gitlab.dev"
+
+    @pytest.mark.asyncio
+    async def test_fetch_uses_proper_endpoint(self, mocker, mock_configuration):
+        mocked_fetch = mocker.patch.object(
+            GitlabEnterprise, "fetch", return_value=Future()
+        )
+        mocked_fetch.return_value.set_result(mocker.MagicMock(body="{}"))
+        mock_configuration._params["gitlab_enterprise"] = {
+            "url": "https://gitlab-enterprise.codecov.dev",
+            "api_url": "https://api.gitlab.dev",
+        }
+        gl = GitlabEnterprise(
+            repo=dict(service_id="187725", name="codecov-test"),
+            owner=dict(username="stevepeak", service_id="109479"),
+            token=dict(key="fake_token"),
+        )
+        res = await gl.post_comment("pullid", "body")
+        assert res == {}
+        mocked_fetch.assert_called_with(
+            "https://api.gitlab.dev/projects/187725/merge_requests/pullid/notes",
+            body='{"body": "body"}',
+            ca_certs=None,
+            connect_timeout=10,
+            headers={
+                "Accept": "application/json",
+                "Authorization": "Bearer fake_token",
+                "Content-Type": "application/json",
+                "User-Agent": "Default",
+            },
+            method="POST",
+            request_timeout=30,
+            validate_cert=None,
+        )
