a22edd4984dd4f5b9fc3ae6c701062d1c59e27ec
Sabreen Noah <sabreen.noah@gmail.com>
2020-05-28 02:23:39 -0700

    Add unit test

--- tests/unit/torngit/test_gitlab.py
+++ tests/unit/torngit/test_gitlab.py
@@ -1,6 +1,7 @@
 import socket
-
 import pytest
+from asyncio import Future
+from json import dumps
 
 from shared.torngit.gitlab import Gitlab
 from shared.torngit.exceptions import TorngitServerUnreachableError
@@ -9,8 +10,8 @@ from shared.torngit.exceptions import TorngitServerUnreachableError
 @pytest.fixture
 def valid_handler():
     return Gitlab(
-        repo=dict(name="example-python"),
-        owner=dict(username="ThiagoCodecov"),
+        repo=dict(service_id="187725", name="codecov-test"),
+        owner=dict(username="ThiagoCodecov", service_id="109479"),
         token=dict(key="some_key"),
     )
 
@@ -21,3 +22,28 @@ class TestUnitGitlab(object):
         mocker.patch.object(Gitlab, "fetch", side_effect=socket.gaierror)
         with pytest.raises(TorngitServerUnreachableError):
             await valid_handler.api("get", "url")
+
+    @pytest.mark.asyncio
+    async def test_get_commit_statuses(self, mocker, valid_handler):
+        mocked_fetch = mocker.patch.object(Gitlab, "fetch", return_value=Future())
+        mocked_fetch.return_value.set_result(
+            mocker.MagicMock(
+                headers={"Content-Type": "application/json"},
+                body=dumps(
+                    [
+                        {
+                            "status": "success",
+                            "description": "Successful status",
+                            "target_url": "url",
+                            "name": "name",
+                            "finished_at": None,
+                            "created_at": None,
+                        }
+                    ]
+                ),
+            )
+        )
+        res = await valid_handler.get_commit_statuses(
+            "c739768fcac68144a3a6d82305b9c4106934d31a"
+        )
+        assert res == "success"
