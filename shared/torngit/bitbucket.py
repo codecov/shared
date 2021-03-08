2143ad812243043d6240903ef25917d30a27273d
Thiago Ramos <thiago@codecov.io>
2021-03-08 13:40:30 -0300

    Fixing oauth1 Bitbucket params
    In a recent PR we accidentally removed oauth1 params from the url. (There was no deploy of this yet, so this is fine)
    
    This PR aims to fix it. It also adds a test so we don't accidentally remove the oauth code again

--- shared/torngit/bitbucket.py
+++ shared/torngit/bitbucket.py
@@ -51,7 +51,6 @@ class Bitbucket(TorngitBaseAdapter, OAuthMixin):
         self, client, version, method, path, json=False, body=None, token=None, **kwargs
     ):
         urlpath = "/api/%s.0%s" % (version, path)
-        url = urlunparse(("https", "bitbucket.org", urlpath, "", urlencode(kwargs), ""))
         headers = {
             "Accept": "application/json",
             "User-Agent": os.getenv("USER_AGENT", "Default"),
@@ -65,11 +64,12 @@ class Bitbucket(TorngitBaseAdapter, OAuthMixin):
         else:
             all_args.update(body or {})
         token_to_use = token or self.token
+        oauth_url = urlunparse(("https", "bitbucket.org", urlpath, "", "", ""))
         oauth = self._oauth_request_parameters(
-            url, token_to_use, all_args, method=method.upper()
+            oauth_url, token_to_use, all_args, method=method.upper()
         )
         kwargs.update(oauth)
-
+        url = urlunparse(("https", "bitbucket.org", urlpath, "", urlencode(kwargs), ""))
         kwargs = dict(
             json=body if body is not None and json else None,
             data=body if not json else None,
