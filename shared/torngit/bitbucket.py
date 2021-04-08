7756de39761721d6e38e4a388fdab399e7cc39ae
Thiago Ramos <thiago@codecov.io>
2021-04-07 21:19:35 -0300

    Trying to fix bitbucket get_authenticated
    As described before, the bitbucket previous logic was weird for private repos.
    
    If the repo was private, the only possible response was (True, True) or the TorngitClientError
    
    So if a repo is private and the user can see it, we assume we can edit as well
    
    We don't want to mess with that code too much, since can_view = False might have weird consequences on the API right now. So we are just adding the proper can_edit logic to the specific section.

--- shared/torngit/bitbucket.py
+++ shared/torngit/bitbucket.py
@@ -767,12 +767,22 @@ class Bitbucket(TorngitBaseAdapter, OAuthMixin):
                 await self.api(
                     client, "2", "get", "/repositories/" + self.slug, token=token
                 )
-                return (True, True)
+                response = await self.api(
+                    client,
+                    "2",
+                    "get",
+                    "/user/permissions/repositories",
+                    token=token,
+                    q=f'repository.full_name="{self.slug}"',
+                )
+                repo_permissions = response["values"] or []
+                can_edit = any(
+                    perm["permission"] in ("admin", "write")
+                    for perm in repo_permissions
+                )
+                return (True, can_edit)
             else:
                 # https://developer.atlassian.com/bitbucket/api/2/reference/resource/user/permissions/repositories
-                # I (Thiago) don't undestand why this is not done everywhere
-                # I am also confident sure the code above is giving can_edit to users
-                # who shouldn't have it
                 groups = await self.api(
                     client,
                     "2",
