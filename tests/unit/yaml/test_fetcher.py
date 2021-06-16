a23c95efd462c9944d353856c5d137697385c4e0
Dorian Amouroux <dor.amouroux@gmail.com>
2021-06-16 12:31:03 +0200

    add complementary tests for yaml not found

--- tests/unit/yaml/test_fetcher.py
+++ tests/unit/yaml/test_fetcher.py
@@ -1,6 +1,7 @@
 import pytest
 import mock
 
+from shared.torngit.exceptions import TorngitObjectNotFoundError
 from shared.yaml import (
     determine_commit_yaml_location,
     fetch_current_yaml_from_provider_via_reference,
@@ -34,6 +35,22 @@ class TestYamlSavingService(object):
         )
         res = await determine_commit_yaml_location(commitid, valid_handler)
         assert res == "codecov.yaml"
+
+    @pytest.mark.asyncio
+    async def test_determine_commit_yaml_location_no_yaml(self, mocker):
+        mocked_result = [
+            {"name": ".gitignore", "path": ".gitignore", "type": "file"},
+            {"name": ".travis.yml", "path": ".travis.yml", "type": "file"},
+            {"name": "README.rst", "path": "README.rst", "type": "file"},
+            {"name": "awesome", "path": "awesome", "type": "folder"},
+            {"name": "tests", "path": "tests", "type": "folder"},
+        ]
+        f = mocked_result
+        valid_handler = mocker.MagicMock(
+            list_top_level_files=mock.AsyncMock(return_value=f)
+        )
+        res = await determine_commit_yaml_location(commitid, valid_handler)
+        assert res is None
 
     @pytest.mark.asyncio
     async def test_determine_commit_yaml_location_no_name(self, mocker):
@@ -147,5 +164,47 @@ class TestYamlSavingService(object):
             commitid, valid_handler
         )
         assert res == sample_yaml
+        valid_handler.list_top_level_files.assert_called_with(commitid)
+        valid_handler.get_source.assert_called_with("codecov.yaml", commitid)
+
+    @pytest.mark.asyncio
+    async def test_fetch_commit_yaml_from_no_yaml(self, mocker):
+        mocked_list_files_result = []
+        list_files_future = mocked_list_files_result
+        contents_result = {"content": sample_yaml}
+        contents_result_future = contents_result
+        valid_handler = mocker.MagicMock(
+            list_top_level_files=mock.AsyncMock(return_value=list_files_future),
+            get_source=mock.AsyncMock(return_value=contents_result_future),
+        )
+        res = await fetch_current_yaml_from_provider_via_reference(
+            commitid, valid_handler
+        )
+        assert res == None
+        valid_handler.list_top_level_files.assert_called_with(commitid)
+
+    @pytest.mark.asyncio
+    async def test_fetch_commit_yaml_from_no_yaml_on_provider(self, mocker):
+        mocked_list_files_result = [
+            {"name": ".gitignore", "path": ".gitignore", "type": "file"},
+            {"name": ".travis.yml", "path": ".travis.yml", "type": "file"},
+            {"name": "README.rst", "path": "README.rst", "type": "file"},
+            {"name": "awesome", "path": "awesome", "type": "folder"},
+            {"name": "codecov", "path": "codecov", "type": "file"},
+            {"name": "codecov.yaml", "path": "codecov.yaml", "type": "file"},
+            {"name": "tests", "path": "tests", "type": "folder"},
+        ]
+        list_files_future = mocked_list_files_result
+        contents_result = {"content": sample_yaml}
+        contents_result_future = contents_result
+        exception_get_source = TorngitObjectNotFoundError("not found", ":(")
+        valid_handler = mocker.MagicMock(
+            list_top_level_files=mock.AsyncMock(return_value=list_files_future),
+            get_source=mock.AsyncMock(side_effect=exception_get_source),
+        )
+        res = await fetch_current_yaml_from_provider_via_reference(
+            commitid, valid_handler
+        )
+        assert res == None
         valid_handler.list_top_level_files.assert_called_with(commitid)
         valid_handler.get_source.assert_called_with("codecov.yaml", commitid)
