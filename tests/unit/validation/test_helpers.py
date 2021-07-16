70b7710fef2dd3f3f1355385d91da247c881cd94
Thiago Ramos <thiago@codecov.io>
2021-07-15 21:00:11 -0300

    Enforcing secrets to be used on same repository
    A long time ago, yaml secrets ( https://docs.codecov.com/docs/codecov-yaml#how-do-i-encrypt-data-that-i-do-not-want-public ) could be created on a specific repo page. And we enforced that repos created on a given repo were used only on that repo.
    
    During the porting, we accidentally ended up relaxing that requirement.
    
    Here is how a malicious party can abuse this:
    1) Find a public repo using codecov secrets in their codecov.yml
    2) Copy the secrets to your own codecov.yml
    3) Spin a codecov enterprise instance
    4) Make upload from that repository to the enterprise instance
    5) Watch the logs show the decrypted value
    
    Here are some reasons why the attack is not easily doable/less valuable:
    1) The attacker needs previous access to the secret in the codecov.yml, which usually means a public repository
    2) The attacker needs to be willing to spin an enterprise instance( we don't show decrypted secrets in the UI)
    3) The attacker needs to know that the secret leaks into the logs after decrypted
    4) Very few customers use this feature
    5) From looking at usage, most times this feature encrypts Slack channel webhooks (which do not allow reading of anything), which are not as valuable except for sending unauthorized messages, and is immediately noticed by the channel users.
    
    The above reasons make it such attack not as valuable. But we should still at least implement this the way the feature was designed.
    
    To be clear, an enterprise attacker that knows how this works can still manipulate the values in the db to make their repo look like the right values fro trigger a decryption of the secret. So this is still not entirely secure

--- tests/unit/validation/test_helpers.py
+++ tests/unit/validation/test_helpers.py
@@ -279,16 +279,19 @@ class TestUserGivenSecret(BaseTestCase):
     def test_simple_user_given_secret(self):
         value = "github/11934774/154468867/https://hooks.slack.com/services/first_key/BE7FWCVHV/dkbfscprianc7wrb"
         encoded_value = UserGivenSecret.encode(value)
-        ugs = UserGivenSecret(show_secret=True)
+        ugs = UserGivenSecret(show_secrets_for=("github", "11934774", "154468867"))
         assert ugs.validate(value) == value
         assert (
             ugs.validate(encoded_value)
             == "https://hooks.slack.com/services/first_key/BE7FWCVHV/dkbfscprianc7wrb"
         )
+        bad_ugs = UserGivenSecret(show_secrets_for=("github", "12345", "154468867"))
+        assert bad_ugs.validate(value) == value
+        assert bad_ugs.validate(encoded_value) == encoded_value
 
     def test_simple_user_given_secret_rotated_key(self):
         encoded_data = "secret:v1::zsV9A8pHadNle357DGJHbZCTyCYA+TXdUd9TN3IY2DIWcPOtgK3Pg1EgA6OZr9XJ1EsdpL765yWrN4pfR3elRdN2LUwiuv6RkNjpbiruHx45agsgxdu8fi24p5pkCLvjcW0HqdH2PTvmHauIp+ptgA=="
-        ugs = UserGivenSecret(show_secret=True)
+        ugs = UserGivenSecret(show_secrets_for=("github", 11934774, 154468867))
         assert (
             ugs.validate(encoded_data)
             == "https://hooks.slack.com/services/first_key/BE7FWCVHV/dkbfscprianc7wrb"
@@ -296,19 +299,19 @@ class TestUserGivenSecret(BaseTestCase):
 
     def test_pseudosecret_user_given_secret(self):
         value = "secret:arriba"
-        ugs = UserGivenSecret(show_secret=True)
+        ugs = UserGivenSecret(show_secrets_for=("github", "12", 98))
         assert ugs.validate(value) == value
 
     def test_b64encoded_pseudosecret_user_given_secret(self):
         encoded_value = b64encode("arriba".encode())
         value = b"secret:" + encoded_value
         value = value.decode()
-        ugs = UserGivenSecret(show_secret=True)
+        ugs = UserGivenSecret(show_secrets_for=("github", "12", 98))
         assert ugs.validate(value) == value
 
     def test_simple_user_dont_show_secret(self):
         value = "github/11934774/154468867/https://hooks.slack.com/services/first_key/BE7FWCVHV/dkbfscprianc7wrb"
         encoded_value = UserGivenSecret.encode(value)
-        ugs = UserGivenSecret(show_secret=False)
+        ugs = UserGivenSecret(show_secrets_for=None)
         assert ugs.validate(value) == value
         assert ugs.validate(encoded_value) == encoded_value
