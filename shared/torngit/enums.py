107e8a7bc300eb27d842c4b0bc1c99e49ffdd9cc
Gguidini <giovanni@codecov.io>
2022-06-07 11:42:00 +0100

    remove TypedDict. use @dataclass instead

--- shared/torngit/enums.py
+++ shared/torngit/enums.py
@@ -1,17 +1,17 @@
 from enum import Enum, auto
 from typing import Callable, Optional
-from mypy_extensions import TypedDict
+from dataclasses import dataclass
 
 
 class Endpoints(Enum):
     commit_detail = auto()
 
 
-class OauthConsumerToken(TypedDict):
+@dataclass
+class OauthConsumerToken:
     key: str
     secret: str
     refresh_token: str
-    redirect_uri_no_protocol: Optional[str]
 
 
 OnRefreshCallback = Optional[Callable[[OauthConsumerToken], None]]
