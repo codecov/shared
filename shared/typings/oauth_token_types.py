18078dce22bba927596d89ad98b91d7cb4d91119
Gguidini <giovanni@codecov.io>
2022-06-27 11:38:11 +0100

    remove typing with dataclass cause it's misleading

--- shared/typings/oauth_token_types.py
+++ shared/typings/oauth_token_types.py
@@ -1,12 +1,13 @@
-from dataclasses import dataclass
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 
-
-@dataclass
-class OauthConsumerToken(object):
-    key: str
-    secret: str
-    refresh_token: str
+# FIXME: This is actually a TypedDict
+# But our CircleCI currently doesn't support Python 3.8, needed for TypedDicts
+# So this will seat here until we update it
+# OauthConsumerToken(TypedDict):
+#   key: str
+#   secret: str
+#   refresh_token: str
+OauthConsumerToken = Any
 
 
 OnRefreshCallback = Optional[Callable[[OauthConsumerToken], None]]
