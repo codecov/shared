f11bd9bd98554a492c6cc828226a9d64e54edaa4
Gguidini <giovanni@codecov.io>
2022-06-27 16:25:33 +0100

    parametrize token tests

--- tests/unit/encryption/test_token.py
+++ tests/unit/encryption/test_token.py
@@ -1,26 +1,51 @@
+import pytest
+
 from shared.encryption.token import decode_token, encode_token
 
 
-def test_encode_access_token():
-    token = {"access_token": "some_key"}
-    assert encode_token(token) == "some_key"
-    token["secret"] = "some_secret"
-    assert encode_token(token) == "some_key:some_secret"
-    token["refresh_token"] = "refresh"
-    assert encode_token(token) == "some_key:some_secret:refresh"
-    token["secret"] = None
-    assert encode_token(token) == "some_key: :refresh"
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ({"access_token": "some_key"}, "some_key"),
+        ({"access_token": "some_key", "secret": "some_secret"}, "some_key:some_secret"),
+        (
+            {
+                "access_token": "some_key",
+                "secret": "some_secret",
+                "refresh_token": "refresh",
+            },
+            "some_key:some_secret:refresh",
+        ),
+        (
+            {"access_token": "some_key", "refresh_token": "refresh"},
+            "some_key: :refresh",
+        ),
+    ],
+)
+def test_encode_access_token(input, expected):
+    assert encode_token(input) == expected
 
 
-def test_decode_access_token():
-    token = {"key": "some_key", "secret": None}
-    assert decode_token("some_key") == token
-    token["secret"] = "some_secret"
-    assert decode_token("some_key:some_secret") == token
-    token["refresh_token"] = "refresh"
-    assert decode_token("some_key:some_secret:refresh") == token
-    token["secret"] = None
-    assert decode_token("some_key: :refresh") == token
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("some_key", {"key": "some_key", "secret": None}),
+        ("some_key:some_secret", {"key": "some_key", "secret": "some_secret"}),
+        (
+            "some_key:some_secret:refresh",
+            {"key": "some_key", "secret": "some_secret", "refresh_token": "refresh"},
+        ),
+        (
+            "some_key: :refresh",
+            {"key": "some_key", "secret": None, "refresh_token": "refresh"},
+        ),
+    ],
+)
+def test_decode_access_token(input, expected):
+    assert decode_token(input) == expected
+
+
+def test_decode_encode_access_token():
     token = {
         "access_token": "some_key",
         "secret": "some_secret",
