a9ed306e44ae2d27a10f50fa4131a6b92cc9a02f
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-06-30 17:07:18 +0200

    hey

--- setup.py
+++ setup.py
@@ -36,7 +36,4 @@ setup(
         "httpx>=0.16.0",
         "oauthlib",
     ],
-    dependency_links=[
-        "git+ssh://git@github.com/codecov/codecov-cli.git@bbbb4ebec1e7cdcf1534deb357b6ef2dc0cada5a#egg=codecov-cli",
-    ],
 )
