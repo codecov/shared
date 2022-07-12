128dbb7f9b3f95c1c36165a342858ba9b6fa37c7
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-07-11 21:52:01 -0300

    Gio/handle refresh token gitlab (#282)
    * extend torngit base to accept token_refresh callback + extend oauth_token type to include refresh_token and redirect_url
    
    * handle refresh tokens for gitlab
    
    * fix TypedDict import cause it doesn't exist in python3.7
    
    * remove TypedDict. use @dataclass instead
    
    * remove the redirect_uri from the token. We can get it from config
    
    * test double refresh. run linter
    
    * extend token encoding to accomodate refresh_token
    
    * Move decode_token to its own file + add encode function + fix imports for OauthToken types.
    
    - Move decode_token to its own file to be reused by different encryptors.
    - Added encode_token function that will be used with refresh_token (by api and worker).
    - Moved OauthToken and OnRefreshCallback typings to a new place to avoid circular import errors (which I got).
      (secretly hoping it will encourage more typings to be created)
    - Some unit tests on the encode / decode of tokens.
    
    * Update to Gitlab token refresh.
    
    - Move refresh if outside of normal 400 errors
    - Change retry style to loop instead of recursion
    - Don't call api on the refresh token function, use httpx directly
    - Throw specific errors from refresh function if it fails OR if we can't do it
    
    * add object to dataclass definition
    
    * deprecates redirect_uri from get_authenticated_user method in favor of self._redirect_uri. Didn't remove it completely to avoid changing the interface right now
    
    * add redirect_uri property to gitlab enterprise
    
    * simplify token encoding logic
    
    * hard code test expected value
    
    * use client to make refresh_token request not directly httpx
    
    * remove cached _redirect_uri from gitlab and gle
    
    * rename creds for clarity
    
    * remove typing with dataclass cause it's misleading
    
    * remove logic from retry loop + remove continue from refresh elif
    
    * update comment so we don't rely on github
    
    * break up redirect tests. Use existent fixture for consistency
    
    * parametrize token tests
    
    * make direct comparison of _token after refresh
    
    * define encode/decode token interface to use 'key' not 'access_token'
    
    * add await to the _on_token_refresh
    
    * gitlab doesn't have self._redirect_uri attribute anymore
    
    * simplify requirements to try and refresh token. Add debug log line when refreshing. Improve type hint for refresh callback
    
    * fix typehint for callback function

--- tests/unit/torngit/test_gitlab.py
+++ tests/unit/torngit/test_gitlab.py
@@ -1,8 +1,19 @@
+import asyncio
+from unittest.mock import MagicMock
+from urllib.parse import parse_qs
+from urllib.request import Request
+
 import httpx
 import pytest
 import respx
 
 from shared.torngit.base import TokenType
+from shared.torngit.exceptions import (
+    TorngitCantRefreshTokenError,
+    TorngitClientGeneralError,
+    TorngitRefreshTokenFailedError,
+    TorngitServer5xxCodeError,
+)
 from shared.torngit.gitlab import Gitlab
 
 
@@ -11,11 +22,34 @@ def valid_handler():
     return Gitlab(
         repo=dict(service_id="187725", name="codecov-test"),
         owner=dict(username="ThiagoCodecov", service_id="109479"),
-        token=dict(key="some_key"),
+        oauth_consumer_token=dict(
+            key="client_id",
+            secret="client_secret",
+        ),
+        token=dict(key="access_token", refresh_token="refresh_token"),
     )
 
 
 class TestUnitGitlab(object):
+    def test_redirect_uri_default(self):
+        gl = Gitlab()
+        assert gl.redirect_uri == "https://codecov.io/login/gitlab"
+
+    def test_redirect_uri_custom_redirect(self, mock_configuration):
+        gl = Gitlab()
+        mock_configuration._params.update(
+            {"gitlab": {"redirect_uri": "https://custom_redirect.com"}}
+        )
+        assert gl.redirect_uri == "https://custom_redirect.com"
+
+    def test_redirect_uri_custom_base(self, mock_configuration):
+        gl = Gitlab()
+
+        mock_configuration._params.update(
+            {"setup": {"codecov_url": "http://localhost"}}
+        )
+        assert gl.redirect_uri == "http://localhost/login/gitlab"
+
     @pytest.mark.asyncio
     async def test_get_commit_statuses(self, mocker, valid_handler):
         mocked_fetch = mocker.patch.object(
@@ -113,7 +147,7 @@ class TestUnitGitlab(object):
         assert res == "pending"
 
     @pytest.mark.asyncio
-    async def test_find_pull_request_by_commit_new_endpoint_doesnt_find_old_does(
+    async def test_find_pull_request_by_commit_endpoint_doesnt_find_old_does(
         self, mocker, valid_handler
     ):
         commit_sha = "c739768fcac68144a3a6d82305b9c4106934d31a"
@@ -174,3 +208,128 @@ class TestUnitGitlab(object):
             )
             await valid_handler.get_source("tests with space.py", "master")
         assert mocked_route.call_count == 1
+
+    @pytest.mark.asyncio
+    async def test_gitlab_refresh_fail_terminates_unavailable(
+        self, mocker, valid_handler
+    ):
+        with pytest.raises(TorngitRefreshTokenFailedError):
+            with respx.mock:
+                mocked_refresh = respx.post("https://gitlab.com/oauth/token").mock(
+                    return_value=httpx.Response(
+                        status_code=502, content="Service unavailable try again later"
+                    )
+                )
+                await valid_handler.refresh_token(valid_handler.get_client())
+        mocked_refresh.call_count == 1
+
+    @pytest.mark.asyncio
+    async def test_gitlab_refresh_fail_terminates_bad_request(
+        self, mocker, valid_handler
+    ):
+        with pytest.raises(TorngitRefreshTokenFailedError):
+            with respx.mock:
+                mocked_refresh = respx.post("https://gitlab.com/oauth/token").mock(
+                    return_value=httpx.Response(
+                        status_code=403, content='{"error": "unauthorized"}'
+                    )
+                )
+                await valid_handler.refresh_token(valid_handler.get_client())
+        mocked_refresh.call_count == 1
+
+    @pytest.mark.asyncio
+    async def test_gitlab_refresh_fail_terminates_bad_request(
+        self, mocker, valid_handler
+    ):
+        valid_handler._token = {"access_token": "old_token_without_refresh"}
+        with pytest.raises(TorngitCantRefreshTokenError):
+            with respx.mock:
+                mocked_refresh = respx.post("https://gitlab.com/oauth/token").mock(
+                    return_value=httpx.Response(
+                        status_code=403, content='{"error": "unauthorized"}'
+                    )
+                )
+                await valid_handler.refresh_token(valid_handler.get_client())
+        mocked_refresh.call_count == 1
+
+    @pytest.mark.asyncio
+    async def test_gitlab_double_refresh(self, mocker, valid_handler):
+        def side_effect(request, *args, **kwargs):
+            parsed_content = parse_qs(request.content)
+            refresh_token = parsed_content[b"refresh_token"][0]
+            if refresh_token == b"refresh_token":
+                return httpx.Response(
+                    status_code=200,
+                    content='{"access_token": "new_access_token","token_type": "bearer","refresh_token": "new_refresh_token"}',
+                )
+            elif refresh_token == b"new_refresh_token":
+                return httpx.Response(
+                    status_code=200,
+                    content='{"access_token": "newer_access_token","token_type": "bearer","refresh_token": "newer_refresh_token"}',
+                )
+            pytest.fail(f"Wrong token received")
+
+        assert valid_handler._oauth == dict(key="client_id", secret="client_secret")
+
+        with respx.mock:
+            mocked_refresh = respx.post("https://gitlab.com/oauth/token").mock(
+                side_effect=side_effect
+            )
+            await valid_handler.refresh_token(valid_handler.get_client())
+            assert mocked_refresh.call_count == 1
+            assert valid_handler._token == dict(
+                key="new_access_token", refresh_token="new_refresh_token"
+            )
+
+            await valid_handler.refresh_token(valid_handler.get_client())
+            assert mocked_refresh.call_count == 2
+            assert valid_handler._token == dict(
+                key="newer_access_token", refresh_token="newer_refresh_token"
+            )
+
+        # Make sure that changing the token doesn't change the _oauth
+        assert valid_handler._oauth == dict(key="client_id", secret="client_secret")
+
+    @pytest.mark.asyncio
+    async def test_gitlab_refresh_after_failed_request(self, mocker, valid_handler):
+        def side_effect(request, *args, **kwargs):
+            token = request.headers["Authorization"]
+            if token == "Bearer access_token":
+                return httpx.Response(
+                    status_code=401,
+                    content='{"error":"invalid_token","error_description":"Token is expired. You can either do re-authorization or token refresh."}',
+                )
+            elif token == "Bearer new_access_token":
+                return httpx.Response(
+                    status_code=200,
+                    content='{"commitid": null, "content": "code goes here"}',
+                )
+            pytest.fail(f"Wrong token received ({token})")
+
+        f = asyncio.Future()
+        f.set_result(True)
+        mock_refresh_callback: MagicMock = mocker.patch.object(
+            valid_handler, "_on_token_refresh", create=True, return_value=f
+        )
+        with respx.mock:
+            mocked_route = respx.get(
+                "https://gitlab.com/api/v4/projects/187725/repository/files/tests%20with%20space.py?ref=master"
+            ).mock(side_effect=side_effect)
+            mocked_refresh = respx.post("https://gitlab.com/oauth/token").mock(
+                return_value=httpx.Response(
+                    status_code=200,
+                    content='{"access_token": "new_access_token","token_type": "bearer","refresh_token": "new_refresh_token"}',
+                )
+            )
+            await valid_handler.get_source("tests with space.py", "master")
+        assert mocked_route.call_count == 2
+        assert mocked_refresh.call_count == 1
+        assert valid_handler._token["key"] == "new_access_token"
+        assert valid_handler._token["refresh_token"] == "new_refresh_token"
+        assert mock_refresh_callback.call_count == 1
+        assert mock_refresh_callback.called_with(
+            {
+                "access_token": "new_access_token",
+                "refresh_token": "new_refresh_token",
+            }
+        )
