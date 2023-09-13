4260c309ce4d43ddb46195ec2f88849d9ae63730
Dana <dana@codecov.io>
2023-09-13 17:51:43 +0300

    add default for is_enterprise, avoid dict assigning bug

--- shared/analytics_tracking/manager.py
+++ shared/analytics_tracking/manager.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from shared.analytics_tracking.base import BaseAnalyticsTool
 from shared.analytics_tracking.events import Event
 
@@ -12,7 +14,17 @@ class AnalyticsToolManager:
     def remove_tool(self, tracking_tool: BaseAnalyticsTool):
         self.tools.remove(tracking_tool)
 
-    def track_event(self, event_name, *, is_enterprise, event_data={}, context=None):
+    def track_event(
+        self,
+        event_name,
+        *,
+        is_enterprise=False,
+        event_data: Optional[dict] = None,
+        context=None
+    ):
+        if event_data is None:
+            event_data = {}
+
         event = Event(event_name, **event_data)
         for tool in self.tools:
             if tool.is_enabled():
