5609e7864764b9d1092ff2b15f8e5161d4756525
trent-codecov <trent@codecov.io>
2023-10-05 00:35:53 -0400

    WIP GHA CI

--- tests/reqs.sh
+++ tests/reqs.sh
@@ -8,3 +8,7 @@ pip install -r tests/requirements.txt
 pip install setuptools_rust
 python setup.py develop
 pip install codecov-cli
+pip install --ignore-installed google-cloud-iam-logging
+pip install --ignore-installed google-cloud
+pip install --ignore-installed google-cloud-storage
+pip install --ignore-installed google-cloud-pubsub
