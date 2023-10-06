0edf8ccdf5c75694507c31547a9974a6f1363c67
trent-codecov <trent@codecov.io>
2023-10-06 14:18:37 -0400

    WIP GHA CI

--- tests/reqs.sh
+++ tests/reqs.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
 python -m venv venv
 source venv/bin/activate
-pip install -r tests/requirements.txt
 pip install setuptools_rust
+pip install -r tests/requirements.txt
+pip install -r requirements.txt
 python setup.py develop
 pip install codecov-cli
-pip install --ignore-installed google-cloud-iam-logging
-pip install --ignore-installed google-cloud
-pip install --ignore-installed google-cloud-storage
-pip install --ignore-installed google-cloud-pubsub
