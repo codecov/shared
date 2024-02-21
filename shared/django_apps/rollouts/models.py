7203d8ff1556fdda55f3911bc976cb141863565d
Daniel Yu <daniel.yu@sentry.io>
2024-02-21 18:36:17 -0500

    add more comments

--- shared/django_apps/rollouts/models.py
+++ shared/django_apps/rollouts/models.py
@@ -4,6 +4,7 @@ from django.contrib.postgres import fields
 from django.db import models
 
 
+# TODO: move to utils
 def default_random_salt():
     chars = []
     ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -13,6 +14,11 @@ def default_random_salt():
 
 
 class FeatureFlag(models.Model):
+    """
+    Represents a feature and its rollout parameters (see shared/rollouts/__init__.py). A
+    default salt will be created if one is not provided.
+    """
+
     name = models.CharField(max_length=200, primary_key=True)
     proportion = models.DecimalField(default=0, decimal_places=3, max_digits=4)
     salt = models.CharField(max_length=10000, default=default_random_salt)
@@ -26,7 +32,15 @@ class FeatureFlag(models.Model):
 
 
 class FeatureFlagVariant(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
+    """
+    Represents a variant of the feature being rolled out and the proportion of
+    the test population it should be rolled out to (see shared/rollouts/__init__.py).
+    The proportion should be a float between 0 and 1. A proportion of 0.5 means 50% of
+    the test population should receive this variant. Ensure that for any `FeatureFlag`,
+    the proportions of the corresponding `FeatureFlagVariant`s sum to 1.
+    """
+
+    name = models.CharField(max_length=200, primary_key=True)  
     feature_flag = models.ForeignKey(
         "FeatureFlag", on_delete=models.CASCADE, related_name="variants"
     )
