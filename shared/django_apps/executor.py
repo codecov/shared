5ca9f0cc83f394b1198a34bf789fc2017aaa29d9
Matt Hammerly <matt.hammerly@sentry.io>
2024-03-04 19:36:09 -0800

    remove prints lol

--- shared/django_apps/executor.py
+++ shared/django_apps/executor.py
@@ -6,15 +6,12 @@ _executor = None
 def get_executor():
     global _executor
     if _executor == None:
-        print("mattmatt initializing threadpool executor")
         _executor = ThreadPoolExecutor(max_workers=1)
-    print("mattmatt returning executor")
     return _executor
 
 
 def run_in_executor(fn):
     def wrapper(*args, **kwargs):
-        print("mattmatt calling wrapper")
         return get_executor().submit(fn, *args, **kwargs).result()
 
     return wrapper
