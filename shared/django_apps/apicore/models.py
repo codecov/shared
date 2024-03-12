91ab80e49073ed7fdc019ec814663eae414595d5
Matt Hammerly <matt.hammerly@sentry.io>
2024-03-12 14:21:42 -0700

    move model definition from apicore to core

--- shared/django_apps/apicore/models.py
+++ shared/django_apps/apicore/models.py
@@ -1,8 +1,14 @@
+import core.models as shared_core
 from django.db import models
 
 
-class Owner(models.Model):
-    username = models.TextField()
+class ProxyOwner(shared_core.Owner):
+    @property
+    def some_property(self):
+        return self.name
+
+    class Meta:
+        proxy = True
 
 
 # Create your models here.
--- shared/django_apps/core/models.py
+++ shared/django_apps/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Owner(models.Model):
+    username = models.TextField()
+
+    class Meta:
+        app_label = "core"
+
+
 # Create your models here.
