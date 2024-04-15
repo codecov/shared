9493b03f9fee8a4db44dd359c3ee773ad851ac51
JerrySentry <jerry.feng@sentry.io>
2024-04-15 11:00:50 -0400

    change import

--- shared/django_apps/dummy_settings.py
+++ shared/django_apps/dummy_settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.contenttypes",
     "django.contrib.postgres",
-    "django.contrib.sessions.models.Session",
+    "django.contrib.sessions",
     "shared.django_apps.codecov_auth",
     "shared.django_apps.core",
     "shared.django_apps.reports",
