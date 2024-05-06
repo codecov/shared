2e43f542dad2917bf2307fc4fec18265a04cb573
Adrian <adrian@codecov.io>
2024-05-06 14:21:45 -0700

    make migrations risky

--- shared/django_apps/user_measurements/migrations/0002_remove_usermeasurement_i_owner_and_more.py
+++ shared/django_apps/user_measurements/migrations/0002_remove_usermeasurement_i_owner_and_more.py
@@ -2,81 +2,156 @@
 
 from django.db import migrations, models
 
+from shared.django_apps.migration_utils import (
+    RiskyAddField,
+    RiskyAddIndex,
+    RiskyRemoveField,
+    RiskyRemoveIndex,
+)
+
 
 class Migration(migrations.Migration):
+    """
+    BEGIN;
+    --
+    -- Remove index i_owner from usermeasurement
+    --
+    DROP INDEX IF EXISTS "i_owner";
+    --
+    -- Remove index owner_repo from usermeasurement
+    --
+    DROP INDEX IF EXISTS "owner_repo";
+    --
+    -- Remove index owner_private_repo from usermeasurement
+    --
+    DROP INDEX IF EXISTS "owner_private_repo";
+    --
+    -- Remove index owner_private_repo_report_type from usermeasurement
+    --
+    DROP INDEX IF EXISTS "owner_private_repo_report_type";
+    --
+    -- Remove field commit from usermeasurement
+    --
+    ALTER TABLE "user_measurements" DROP COLUMN "commit_id" CASCADE;
+    --
+    -- Remove field owner from usermeasurement
+    --
+    ALTER TABLE "user_measurements" DROP COLUMN "owner_id" CASCADE;
+    --
+    -- Remove field repo from usermeasurement
+    --
+    ALTER TABLE "user_measurements" DROP COLUMN "repo_id" CASCADE;
+    --
+    -- Remove field upload from usermeasurement
+    --
+    ALTER TABLE "user_measurements" DROP COLUMN "upload_id" CASCADE;
+    --
+    -- Add field commit_id to usermeasurement
+    --
+    ALTER TABLE "user_measurements" ADD COLUMN "commit_id" integer NULL;
+    --
+    -- Add field owner_id to usermeasurement
+    --
+    ALTER TABLE "user_measurements" ADD COLUMN "owner_id" integer NULL;
+    --
+    -- Add field repo_id to usermeasurement
+    --
+    ALTER TABLE "user_measurements" ADD COLUMN "repo_id" integer NULL;
+    --
+    -- Add field upload_id to usermeasurement
+    --
+    ALTER TABLE "user_measurements" ADD COLUMN "upload_id" integer NULL;
+    --
+    -- Create index i_owner on field(s) owner_id of model usermeasurement
+    --
+    CREATE INDEX "i_owner" ON "user_measurements" ("owner_id");
+    --
+    -- Create index owner_repo on field(s) owner_id, repo_id of model usermeasurement
+    --
+    CREATE INDEX "owner_repo" ON "user_measurements" ("owner_id", "repo_id");
+    --
+    -- Create index owner_private_repo on field(s) owner_id, private_repo of model usermeasurement
+    --
+    CREATE INDEX "owner_private_repo" ON "user_measurements" ("owner_id", "private_repo");
+    --
+    -- Create index owner_private_repo_report_type on field(s) owner_id, private_repo, report_type of model usermeasurement
+    --
+    CREATE INDEX "owner_private_repo_report_type" ON "user_measurements" ("owner_id", "private_repo", "report_type");
+    COMMIT;
+    """
 
     dependencies = [
         ("user_measurements", "0001_initial"),
     ]
 
     operations = [
-        migrations.RemoveIndex(
+        RiskyRemoveIndex(
             model_name="usermeasurement",
             name="i_owner",
         ),
-        migrations.RemoveIndex(
+        RiskyRemoveIndex(
             model_name="usermeasurement",
             name="owner_repo",
         ),
-        migrations.RemoveIndex(
+        RiskyRemoveIndex(
             model_name="usermeasurement",
             name="owner_private_repo",
         ),
-        migrations.RemoveIndex(
+        RiskyRemoveIndex(
             model_name="usermeasurement",
             name="owner_private_repo_report_type",
         ),
-        migrations.RemoveField(
+        RiskyRemoveField(
             model_name="usermeasurement",
             name="commit",
         ),
-        migrations.RemoveField(
+        RiskyRemoveField(
             model_name="usermeasurement",
             name="owner",
         ),
-        migrations.RemoveField(
+        RiskyRemoveField(
             model_name="usermeasurement",
             name="repo",
         ),
-        migrations.RemoveField(
+        RiskyRemoveField(
             model_name="usermeasurement",
             name="upload",
         ),
-        migrations.AddField(
+        RiskyAddField(
             model_name="usermeasurement",
             name="commit_id",
             field=models.IntegerField(null=True),
         ),
-        migrations.AddField(
+        RiskyAddField(
             model_name="usermeasurement",
             name="owner_id",
             field=models.IntegerField(null=True),
         ),
-        migrations.AddField(
+        RiskyAddField(
             model_name="usermeasurement",
             name="repo_id",
             field=models.IntegerField(null=True),
         ),
-        migrations.AddField(
+        RiskyAddField(
             model_name="usermeasurement",
             name="upload_id",
             field=models.IntegerField(null=True),
         ),
-        migrations.AddIndex(
+        RiskyAddIndex(
             model_name="usermeasurement",
             index=models.Index(fields=["owner_id"], name="i_owner"),
         ),
-        migrations.AddIndex(
+        RiskyAddIndex(
             model_name="usermeasurement",
             index=models.Index(fields=["owner_id", "repo_id"], name="owner_repo"),
         ),
-        migrations.AddIndex(
+        RiskyAddIndex(
             model_name="usermeasurement",
             index=models.Index(
                 fields=["owner_id", "private_repo"], name="owner_private_repo"
             ),
         ),
-        migrations.AddIndex(
+        RiskyAddIndex(
             model_name="usermeasurement",
             index=models.Index(
                 fields=["owner_id", "private_repo", "report_type"],
