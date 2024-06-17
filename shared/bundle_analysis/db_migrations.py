60796ee5e667b512489a0db859e0a1b407eff181
JerrySentry <jerry.feng@sentry.io>
2024-06-17 17:50:03 -0400

    add comments

--- shared/bundle_analysis/db_migrations.py
+++ shared/bundle_analysis/db_migrations.py
@@ -3,11 +3,23 @@ from sqlalchemy.orm import Session
 
 
 class BundleAnalysisMigration:
+    """
+    Keeps track of DB schema migrations for the Bundle Analysis Report SQLite file
+    When updating the tables/models in assets.py, the SCHEMA_VERSION needs to be
+    incremented by 1, and an entry in self.migrations needs to correspond the new version
+    and the SQL changes that needs to execute to update to the latest changes.
+    When new bundle analysis reports are processed they will be on the latest version and
+    no migration will be needed, however for commits with reports of older versions, when
+    they are fetched the migrations will be applied to prevent errors with unexpected DB schemas.
+    """
+
     def __init__(self, db_session: Session, from_version: int, to_version: int):
         self.db_session = db_session
         self.from_version = from_version
         self.to_version = to_version
 
+        # Mapping of the schema_version number to the migration function that needs to run
+        # {x: fcn} means to bring version x-1 to x, fcn must be ran
         self.migrations = {2: self.add_gzip_size}
 
     def update_schema_version(self, version):
