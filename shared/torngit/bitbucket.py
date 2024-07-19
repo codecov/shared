0b145c229cbc3c31546ac9565ed358fb0d6961b0
Rohit <rohit.vinnakota@sentry.io>
2024-07-19 12:24:48 -0400

    check test

--- shared/torngit/bitbucket.py
+++ shared/torngit/bitbucket.py
@@ -217,8 +217,6 @@ class Bitbucket(TorngitBaseAdapter):
             groups = await self.api(
                 client, "2", "get", "/user/permissions/workspaces", token=token
             )
-        print("ASJJJAS")
-        print(groups)
         if groups["values"]:
             for group in groups["values"]:
                 if (
