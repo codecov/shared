6439ec85a597055de5aac35495abe0fd9742acf9
Adrian <adrian@codecov.io>
2024-08-15 11:34:48 -0700

    add change

--- shared/bots/github_apps.py
+++ shared/bots/github_apps.py
@@ -13,9 +13,9 @@ from shared.django_apps.codecov_auth.models import (
 )
 from shared.django_apps.core.models import Repository
 from shared.github import get_github_integration_token
+from shared.helpers.redis import get_redis_connection
 from shared.orms.owner_helper import DjangoSQLAlchemyOwnerWrapper
 from shared.rate_limits import determine_if_entity_is_rate_limited, gh_app_key_name
-from shared.helpers.redis import get_redis_connection
 from shared.typings.oauth_token_types import Token
 from shared.typings.torngit import GithubInstallationInfo
 
