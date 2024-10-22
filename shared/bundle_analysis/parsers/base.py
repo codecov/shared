4e2792779e3efc65f876ab8ac38ffd244189e34d
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2024-10-22 11:23:52 +0200

    chore: improve typing and tracing of BA reports (#402)
    Small addition to tracing of some functions related to BA reports.
    And some small fixes so VS Code would stop complaning about types.
    
    ps.: `ParserTrait` might not be the best name for what that class is...
    it's not very pythonic in any case

--- shared/bundle_analysis/parsers/base.py
+++ shared/bundle_analysis/parsers/base.py
@@ -1,7 +1,20 @@
 import abc
+from typing import Tuple
+
+from sqlalchemy.orm import Session
 
 
 class ParserInterface(metaclass=abc.ABCMeta):
     @classmethod
     def __subclasshook__(cls, subclass):
         return hasattr(subclass, "parse") and callable(subclass.parse)
+
+
+class ParserTrait:
+    @abc.abstractmethod
+    def __init__(self, db_session: Session):
+        pass
+
+    @abc.abstractmethod
+    def parse(self, path: str) -> Tuple[int, str]:
+        pass
