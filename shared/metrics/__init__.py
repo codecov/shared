cde98920fbec05ad7f71f934fed35a5eebab4dd3
Tony Le <tony.le@sentry.io>
2024-10-23 11:27:34 -0400

    feat: add inc_counter function to handle errors with metrics (#407)
    * feat: add inc_counter function to handle errors with metrics
    
    * Update shared/metrics/__init__.py
    
    Co-authored-by: michelletran-codecov <167130096+michelletran-codecov@users.noreply.github.com>
    
    * Add newline to EOF
    
    ---------
    
    Co-authored-by: michelletran-codecov <167130096+michelletran-codecov@users.noreply.github.com>

--- shared/metrics/__init__.py
+++ shared/metrics/__init__.py
@@ -1,4 +1,8 @@
+import logging
+
 from prometheus_client import Counter, Histogram, Summary, start_http_server
+
+log = logging.getLogger(__name__)
 
 start_prometheus = start_http_server
 
@@ -9,3 +13,10 @@ __all__ = [
     "Summary",
     "start_prometheus",
 ]
+
+
+def inc_counter(counter: Counter, labels: dict) -> None:
+    try:
+        counter.labels(**labels).inc()
+    except Exception as e:
+        log.warning(f"Error incrementing counter {counter._name}: {e}")
