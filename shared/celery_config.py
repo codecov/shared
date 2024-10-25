aab26870855a544693cf96937fc5f40bd35ce8f5
joseph-sentry <joseph.sawaya@sentry.io>
2024-10-25 13:36:45 -0400

    feat: get shared ready caching test rollups

--- shared/celery_config.py
+++ shared/celery_config.py
@@ -45,6 +45,11 @@ test_results_finisher_task_name = (
 sync_test_results_task_name = (
     f"app.tasks.{TaskConfigGroup.test_results.value}.SyncTestResultsTask"
 )
+
+cache_test_rollups_task_name = (
+    f"app.tasks.{TaskConfigGroup.test_results.value}.CacheTestRollupsTask"
+)
+
 process_flakes_task_name = f"app.tasks.{TaskConfigGroup.flakes.value}.ProcessFlakesTask"
 
 manual_upload_completion_trigger_task_name = (
