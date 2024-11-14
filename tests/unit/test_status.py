bb18502a2446a5a576fcda4e2ad410187bf06869
Arpad Borsos <arpad.borsos@sentry.io>
2024-11-14 17:11:25 +0100

    Remove some unneeded dependencies (#425)
    This removes a bunch of dependencies which were only needed for forward compatibility across Python versions, as well as some very niche dependencies with well-established alternatives.

--- tests/unit/test_status.py
+++ tests/unit/test_status.py
@@ -1,13 +1,11 @@
-import unittest
-
-from ddt import data, ddt
+import pytest
 
 from shared.torngit.status import Status
 
 
-@ddt
-class Test(unittest.TestCase):
-    @data(
+@pytest.mark.parametrize(
+    "statuses, res, why",
+    [
         (
             [
                 {
@@ -56,83 +54,16 @@ class Test(unittest.TestCase):
             "pending",
             "newest prevails",
         ),
-    )
-    def test_str(self, statuses_res_why):
-        statuses, res, why = statuses_res_why
-        assert str(Status(statuses)) == res, why
+    ],
+)
+def test_str(statuses, res, why):
+    assert str(Status(statuses)) == res, why
 
-    @data(("demo/*", 2), ("demo/a", 3), ("blah", 4))
-    def test_sub(self, out_l):
-        out, li = out_l
-        s = Status(
-            [
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "demo/a",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "demo/b",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "ci",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "other",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-            ]
-        )
-        new = s - out
-        assert id(new) != id(s), "original should not be modified"
-        assert len(new) == li
-        assert out not in new
 
-    def test_status_with_none_time(self):
-        s = Status(
-            [
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "demo/a",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-                {"url": None, "state": "pending", "context": "demo/b", "time": None},
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "demo/b",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "ci",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-                {
-                    "url": None,
-                    "state": "pending",
-                    "context": "other",
-                    "time": "2015-12-21T16:54:13Z",
-                },
-            ]
-        )
-        assert len(s) == 4
-        new = s - "demo/*"
-        assert len(new) == 2
-
-    def test_fetch_most_relevant_status_per_context(self):
-        statuses = [
+@pytest.mark.parametrize("out, li", [("demo/*", 2), ("demo/a", 3), ("blah", 4)])
+def test_sub(out, li):
+    s = Status(
+        [
             {
                 "url": None,
                 "state": "pending",
@@ -141,7 +72,36 @@ class Test(unittest.TestCase):
             },
             {
                 "url": None,
-                "state": "success",
+                "state": "pending",
+                "context": "demo/b",
+                "time": "2015-12-21T16:54:13Z",
+            },
+            {
+                "url": None,
+                "state": "pending",
+                "context": "ci",
+                "time": "2015-12-21T16:54:13Z",
+            },
+            {
+                "url": None,
+                "state": "pending",
+                "context": "other",
+                "time": "2015-12-21T16:54:13Z",
+            },
+        ]
+    )
+    new = s - out
+    assert id(new) != id(s), "original should not be modified"
+    assert len(new) == li
+    assert out not in new
+
+
+def test_status_with_none_time():
+    s = Status(
+        [
+            {
+                "url": None,
+                "state": "pending",
                 "context": "demo/a",
                 "time": "2015-12-21T16:54:13Z",
             },
@@ -160,32 +120,72 @@ class Test(unittest.TestCase):
             },
             {
                 "url": None,
-                "state": "success",
-                "context": "ci",
-                "time": "2010-12-21T16:54:13Z",
-            },
-            {"url": None, "state": "pending", "context": "other", "time": None},
-        ]
-        res = Status._fetch_most_relevant_status_per_context(statuses)
-        expected_res = [
-            {
-                "context": "demo/a",
-                "state": "success",
-                "time": "2015-12-21T16:54:13Z",
-                "url": None,
-            },
-            {
-                "context": "demo/b",
                 "state": "pending",
+                "context": "other",
                 "time": "2015-12-21T16:54:13Z",
-                "url": None,
             },
-            {
-                "context": "ci",
-                "state": "pending",
-                "time": "2015-12-21T16:54:13Z",
-                "url": None,
-            },
-            {"context": "other", "state": "pending", "time": None, "url": None},
         ]
-        assert expected_res == res
+    )
+    assert len(s) == 4
+    new = s - "demo/*"
+    assert len(new) == 2
+
+
+def test_fetch_most_relevant_status_per_context():
+    statuses = [
+        {
+            "url": None,
+            "state": "pending",
+            "context": "demo/a",
+            "time": "2015-12-21T16:54:13Z",
+        },
+        {
+            "url": None,
+            "state": "success",
+            "context": "demo/a",
+            "time": "2015-12-21T16:54:13Z",
+        },
+        {"url": None, "state": "pending", "context": "demo/b", "time": None},
+        {
+            "url": None,
+            "state": "pending",
+            "context": "demo/b",
+            "time": "2015-12-21T16:54:13Z",
+        },
+        {
+            "url": None,
+            "state": "pending",
+            "context": "ci",
+            "time": "2015-12-21T16:54:13Z",
+        },
+        {
+            "url": None,
+            "state": "success",
+            "context": "ci",
+            "time": "2010-12-21T16:54:13Z",
+        },
+        {"url": None, "state": "pending", "context": "other", "time": None},
+    ]
+    res = Status._fetch_most_relevant_status_per_context(statuses)
+    expected_res = [
+        {
+            "context": "demo/a",
+            "state": "success",
+            "time": "2015-12-21T16:54:13Z",
+            "url": None,
+        },
+        {
+            "context": "demo/b",
+            "state": "pending",
+            "time": "2015-12-21T16:54:13Z",
+            "url": None,
+        },
+        {
+            "context": "ci",
+            "state": "pending",
+            "time": "2015-12-21T16:54:13Z",
+            "url": None,
+        },
+        {"context": "other", "state": "pending", "time": None, "url": None},
+    ]
+    assert expected_res == res
