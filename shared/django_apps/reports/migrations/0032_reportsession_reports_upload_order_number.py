138f2617dce1f1b100af99b9f09c9a062c9af440
Adrian <adrian@codecov.io>
2024-11-21 17:54:40 -0700

    lint

--- shared/django_apps/reports/migrations/0032_reportsession_reports_upload_order_number.py
+++ shared/django_apps/reports/migrations/0032_reportsession_reports_upload_order_number.py
@@ -4,15 +4,18 @@ from django.db import migrations, models
 
 from shared.django_apps.migration_utils import RiskyAddIndex
 
-class Migration(migrations.Migration):
 
+class Migration(migrations.Migration):
     dependencies = [
-        ('reports', '0031_lastcacherollupdate_and_more'),
+        ("reports", "0031_lastcacherollupdate_and_more"),
     ]
 
     operations = [
         RiskyAddIndex(
-            model_name='reportsession',
-            index=models.Index(fields=['report_id', 'upload_type', 'order_number'], name='reports_upload_order_number_upload_type_report_id_index'),
+            model_name="reportsession",
+            index=models.Index(
+                fields=["report_id", "upload_type", "order_number"],
+                name="reports_upload_order_number_upload_type_report_id_index",
+            ),
         ),
     ]
