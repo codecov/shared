ee6e4b42f8932f5b5b6bb4b1d2256de17efad0d2
RulaKhaled <rola.abuhasna@sentry.io>
2024-11-26 12:52:32 +0100

    Refactor tests

--- tests/unit/billing/test_enum_definitions.py
+++ tests/unit/billing/test_enum_definitions.py
@@ -1,61 +1,99 @@
+import pytest
 from django.test import override_settings
 
 from shared.billing import BillingPlan, is_enterprise_cloud_plan, is_pr_billing_plan
+from shared.django_apps.codecov_auth.tests.factories import OwnerFactory
 
 
-def test_billing_enums():
-    assert BillingPlan.users_monthly.db_name == "users-inappm"
-    assert BillingPlan.users_yearly.db_name == "users-inappy"
-    assert BillingPlan.users_free.db_name == "users-free"
-    assert BillingPlan.users_basic.db_name == "users-basic"
-    assert BillingPlan.pr_monthly.db_name == "users-pr-inappm"
-    assert BillingPlan.pr_yearly.db_name == "users-pr-inappy"
-    assert BillingPlan.enterprise_cloud_yearly.db_name == "users-enterprisey"
-    assert BillingPlan.enterprise_cloud_monthly.db_name == "users-enterprisem"
-    assert BillingPlan.team_monthly.db_name == "users-teamm"
-    assert BillingPlan.team_yearly.db_name == "users-teamy"
+class TestBillingServiceTestCase(object):
+    @override_settings(IS_ENTERPRISE=True)
+    @pytest.fixture
+    def dbsession(self, db):
+        return db
 
+    @override_settings(IS_ENTERPRISE=False)
+    def test_pr_author_plan_check(self, request, dbsession, mocker, mock_configuration):
+        owner = OwnerFactory(service="github", plan="users-pr-inappm")
+        if dbsession is not None:
+            dbsession.add(owner)
+            dbsession.flush()
+        assert is_pr_billing_plan(owner.plan)
 
-def test_get_from_string():
-    assert BillingPlan.from_str("users-inappm") == BillingPlan.users_monthly
-    assert BillingPlan.from_str("users-inappy") == BillingPlan.users_yearly
-    assert BillingPlan.from_str("users-free") == BillingPlan.users_free
-    assert BillingPlan.from_str("users-basic") == BillingPlan.users_basic
-    assert BillingPlan.from_str("users-pr-inappm") == BillingPlan.pr_monthly
-    assert BillingPlan.from_str("users-pr-inappy") == BillingPlan.pr_yearly
-    assert (
-        BillingPlan.from_str("users-enterprisey") == BillingPlan.enterprise_cloud_yearly
-    )
-    assert (
-        BillingPlan.from_str("users-enterprisem")
-        == BillingPlan.enterprise_cloud_monthly
-    )
-    assert BillingPlan.from_str("users-teamm") == BillingPlan.team_monthly
-    assert BillingPlan.from_str("users-teamy") == BillingPlan.team_yearly
+    @override_settings(IS_ENTERPRISE=True)
+    def test_pr_author_enterprise_plan_check(
+        self, request, dbsession, mocker, mock_configuration
+    ):
+        owner = OwnerFactory(service="github")
+        if dbsession is not None:
+            dbsession.add(owner)
+            dbsession.flush()
 
+        encrypted_license = "wxWEJyYgIcFpi6nBSyKQZQeaQ9Eqpo3SXyUomAqQOzOFjdYB3A8fFM1rm+kOt2ehy9w95AzrQqrqfxi9HJIb2zLOMOB9tSy52OykVCzFtKPBNsXU/y5pQKOfV7iI3w9CHFh3tDwSwgjg8UsMXwQPOhrpvl2GdHpwEhFdaM2O3vY7iElFgZfk5D9E7qEnp+WysQwHKxDeKLI7jWCnBCBJLDjBJRSz0H7AfU55RQDqtTrnR+rsLDHOzJ80/VxwVYhb"
+        mock_configuration.params["setup"]["enterprise_license"] = encrypted_license
+        mock_configuration.params["setup"]["codecov_dashboard_url"] = (
+            "https://codecov.mysite.com"
+        )
 
-def test_is_enterprise_cloud_plan():
-    assert not is_enterprise_cloud_plan(BillingPlan.pr_monthly)
-    assert not is_enterprise_cloud_plan(BillingPlan.pr_yearly)
-    assert is_enterprise_cloud_plan(BillingPlan.enterprise_cloud_yearly)
-    assert is_enterprise_cloud_plan(BillingPlan.enterprise_cloud_monthly)
+        assert is_pr_billing_plan(owner.plan)
 
+    @override_settings(IS_ENTERPRISE=False)
+    def test_plan_not_pr_author(self, request, dbsession, mocker, mock_configuration):
+        owner = OwnerFactory(service="github", plan=BillingPlan.users_monthly.value)
+        if dbsession is not None:
+            dbsession.add(owner)
+            dbsession.flush()
 
-@override_settings(IS_ENTERPRISE=True)
-def test_is_pr_billing_plan():
-    assert is_pr_billing_plan(BillingPlan.pr_monthly)
-    assert is_pr_billing_plan(BillingPlan.pr_yearly)
-    assert not is_pr_billing_plan(BillingPlan.enterprise_cloud_yearly)
-    assert not is_pr_billing_plan(BillingPlan.enterprise_cloud_monthly)
-    assert not is_pr_billing_plan(BillingPlan.team_monthly)
-    assert not is_pr_billing_plan(BillingPlan.team_yearly)
+        assert not is_pr_billing_plan(owner.plan)
 
+    @override_settings(IS_ENTERPRISE=True)
+    def test_pr_author_enterprise_plan_check_non_pr_plan(
+        self, request, dbsession, mocker, mock_configuration
+    ):
+        owner = OwnerFactory(service="github")
+        if dbsession is not None:
+            dbsession.add(owner)
+            dbsession.flush()
 
-@override_settings(IS_ENTERPRISE=False)
-def test_is_pr_billing_plan_false():
-    assert not is_pr_billing_plan(BillingPlan.pr_monthly)
-    assert not is_pr_billing_plan(BillingPlan.pr_yearly)
-    assert is_pr_billing_plan(BillingPlan.users_free)
-    assert is_pr_billing_plan(BillingPlan.users_basic)
-    assert is_pr_billing_plan(BillingPlan.users_trial)
-    assert is_pr_billing_plan(BillingPlan.users_ghm)
+        encrypted_license = "0dRbhbzp8TVFQp7P4e2ES9lSfyQlTo8J7LQ"
+        mock_configuration.params["setup"]["enterprise_license"] = encrypted_license
+        mock_configuration.params["setup"]["codecov_dashboard_url"] = (
+            "https://codeov.mysite.com"
+        )
+
+        assert not is_pr_billing_plan(owner.plan)
+
+    def test_billing_enums(self):
+        assert BillingPlan.users_monthly.db_name == "users-inappm"
+        assert BillingPlan.users_yearly.db_name == "users-inappy"
+        assert BillingPlan.users_free.db_name == "users-free"
+        assert BillingPlan.users_basic.db_name == "users-basic"
+        assert BillingPlan.pr_monthly.db_name == "users-pr-inappm"
+        assert BillingPlan.pr_yearly.db_name == "users-pr-inappy"
+        assert BillingPlan.enterprise_cloud_yearly.db_name == "users-enterprisey"
+        assert BillingPlan.enterprise_cloud_monthly.db_name == "users-enterprisem"
+        assert BillingPlan.team_monthly.db_name == "users-teamm"
+        assert BillingPlan.team_yearly.db_name == "users-teamy"
+
+    def test_get_from_string(self):
+        assert BillingPlan.from_str("users-inappm") == BillingPlan.users_monthly
+        assert BillingPlan.from_str("users-inappy") == BillingPlan.users_yearly
+        assert BillingPlan.from_str("users-free") == BillingPlan.users_free
+        assert BillingPlan.from_str("users-basic") == BillingPlan.users_basic
+        assert BillingPlan.from_str("users-pr-inappm") == BillingPlan.pr_monthly
+        assert BillingPlan.from_str("users-pr-inappy") == BillingPlan.pr_yearly
+        assert (
+            BillingPlan.from_str("users-enterprisey")
+            == BillingPlan.enterprise_cloud_yearly
+        )
+        assert (
+            BillingPlan.from_str("users-enterprisem")
+            == BillingPlan.enterprise_cloud_monthly
+        )
+        assert BillingPlan.from_str("users-teamm") == BillingPlan.team_monthly
+        assert BillingPlan.from_str("users-teamy") == BillingPlan.team_yearly
+
+    def test_is_enterprise_cloud_plan(self):
+        assert not is_enterprise_cloud_plan(BillingPlan.pr_monthly)
+        assert not is_enterprise_cloud_plan(BillingPlan.pr_yearly)
+        assert is_enterprise_cloud_plan(BillingPlan.enterprise_cloud_yearly)
+        assert is_enterprise_cloud_plan(BillingPlan.enterprise_cloud_monthly)
