8b175d7023eabd7abcfa076238d0fa60dbf23e83
Spencer Murray <spencer.murray@sentry.io>
2025-01-28 15:35:08 -0500

    Iteration 2.5

--- shared/events/amplitude.py
+++ shared/events/amplitude.py
@@ -1,5 +1,5 @@
 from typing import Literal, TypedDict
-from shared.events.base import EventPublisher, EventPublisherPropertyError
+from shared.events.base import EventPublisher, EventPublisherPropertyException
 from amplitude import Amplitude, BaseEvent, Config, EventOptions
 
 AMPLITUDE_API_KEY = None
@@ -11,13 +11,24 @@ type AmplitudeEventType = Literal[
     "set_orgs" # special event for setting a user's member orgs
 ]
 
-# Separate type required to make user_owner_id mandatory, but all others optional
+type AmplitudeEventProperty = Literal[
+    "user_ownerid",
+    "ownerid",
+    "org_ids",
+]
+
+# Separate type required to make user_ownerid mandatory, but all others optional
 class BaseAmplitudeEventProperties(TypedDict, total=True):
-    user_owner_id: int # ownerid of user performing event action
+    user_ownerid: int # ownerid of user performing event action
 
 class AmplitudeEventProperties(BaseAmplitudeEventProperties, total=False):
     ownerid: int # ownerid of owner being acted upon
     org_ids: list[str]
+
+# user_ownerid is always required, don't need to check here.
+amplitude_required_properties: dict[AmplitudeEventType, list[AmplitudeEventProperty]] = {
+    'App Installed': ['ownerid']
+}
 
 class AmplitudeEventPublisher(EventPublisher):
     client: Amplitude
@@ -31,21 +42,35 @@ class AmplitudeEventPublisher(EventPublisher):
         ))
 
     def publish(self, event_type: AmplitudeEventType, event_properties: AmplitudeEventProperties):
+        # Handle special set_orgs event
         if event_type == "set_orgs":
             if 'org_ids' not in event_properties:
-                raise EventPublisherPropertyError("event_type 'set_orgs' requires property 'org_ids'")
+                raise EventPublisherPropertyException("event_type 'set_orgs' requires property 'org_ids'")
 
             self.client.set_group(
                 group_type="org", 
                 group_name=[str(orgid) for orgid in event_properties['org_ids']], 
-                event_options=EventOptions(user_id=str(event_properties['user_owner_id']))
+                event_options=EventOptions(user_id=str(event_properties['user_ownerid']))
             )
+            return
 
+        # Handle normal events
+        structured_payload = transform_properties(event_type, event_properties)
 
+        # Track event with validated payload, we will raise an exception before
+        # this if bad payload.
         self.client.track(
-            BaseEvent(event_type, user_id=str(event_properties['user_owner_id']), event_properties=dict(event_properties))
+            BaseEvent(event_type, user_id=str(event_properties['user_ownerid']), event_properties=structured_payload)
         )
+        return
 
-a = AmplitudeEventPublisher()
+def transform_properties(event_type: AmplitudeEventType, event_properties: AmplitudeEventProperties) -> dict:
+    payload = {}
 
-a.publish('set_orgs', {"user_owner_id": 2, "org_ids": ['1', '2']})
+    for property in amplitude_required_properties[event_type]:
+        if property not in event_properties:
+            raise EventPublisherPropertyException(f"Property {property} is required for event type {event_type}")
+        payload[property] = event_properties.get(property)
+
+    return payload
+
--- shared/events/base.py
+++ shared/events/base.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-class EventPublisherPropertyError(Exception):
+class EventPublisherPropertyException(Exception):
     pass
 
 class EventPublisher[T, P](ABC):
