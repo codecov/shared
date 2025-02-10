69b736859ab71e1e3e91018439a6cb6b792502f3
JerrySentry <142266253+JerrySentry@users.noreply.github.com>
2025-02-10 12:37:32 -0500

    Bundle Analysis: add a getter for cache config (#506)
    * Bundle Analysis: add a getter for cache config
    
    * add typing

--- shared/django_apps/bundle_analysis/service/bundle_analysis.py
+++ shared/django_apps/bundle_analysis/service/bundle_analysis.py
@@ -3,13 +3,20 @@ from shared.django_apps.bundle_analysis.models import CacheConfig
 
 class BundleAnalysisCacheConfigService:
     @staticmethod
-    def update_cache_option(repo_id, name, is_caching=True) -> None:
+    def update_cache_option(repo_id: int, name: str, is_caching: bool = True) -> None:
         CacheConfig.objects.update_or_create(
             repo_id=repo_id, bundle_name=name, defaults={"is_caching": is_caching}
         )
 
     @staticmethod
-    def create_if_not_exists(repo_id, name, is_caching=True) -> None:
+    def create_if_not_exists(repo_id: int, name: str, is_caching: bool = True) -> None:
         CacheConfig.objects.get_or_create(
             repo_id=repo_id, bundle_name=name, defaults={"is_caching": is_caching}
         )
+
+    @staticmethod
+    def get_cache_option(repo_id: int, name: str) -> bool:
+        cache_option = CacheConfig.objects.filter(
+            repo_id=repo_id, bundle_name=name
+        ).first()
+        return cache_option.is_caching if cache_option else False
