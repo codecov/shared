b892ed7251de86d7b9c984ab641d0bb5cca672b8
Spencer Murray <spencer.murray@sentry.io>
2025-02-18 09:09:30 -0500

    Add event property types for coverage upload event

--- shared/events/amplitude/publisher.py
+++ shared/events/amplitude/publisher.py
@@ -118,7 +118,9 @@ class StubbedAmplitudeClient(Amplitude):
         group_name: Union[str, list[str]],
         event_options: EventOptions,
     ):
+        log.info(f'StubbedAmplitudeClient set_group {group_type}: {group_name}', extra=event_options.get_event_body())
         return
 
     def track(self, event: BaseEvent):
+        log.info(f'StubbedAmplitudeClient tracked event {event.event_type}', extra=event.get_event_body())
         return
