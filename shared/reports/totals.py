88470fc1699797cbf7c01e04613e50205d6e6f1f
Arpad Borsos <arpad.borsos@sentry.io>
2025-02-21 14:23:17 +0100

    Consolidate implementation of `_process_totals`
    In particular, this unifies the duplicated implementations of these methods across `Filtered/ReportFile`.
    
    The new method also spells out a "very concise, but also unreadable" implementation that sums up the cyclomatic complexity (something I would like to remove completely eventually).
    This is now more readable and understandable, but more importantly, it only iterates over the `lines` *once*, instead of twice as the previous implementation did.
    Along with avoiding a bunch of intermediate list allocations that the `FilteredReportFile` implementation did, this should yield quite a good speedup.

---
+++ shared/reports/totals.py
@@ -0,0 +1,59 @@
+from typing import Iterator
+
+from shared.helpers.numeric import ratio
+from shared.reports.types import ReportLine, ReportTotals
+from shared.utils.merge import LineType, line_type
+
+
+def get_line_totals(lines: Iterator[ReportLine]) -> ReportTotals:
+    """
+    Calculates the totals (`ReportTotals`) across all the given `lines` (`ReportLine`s).
+    """
+    hits = 0
+    misses = 0
+    partials = 0
+    branches = 0
+    methods = 0
+    messages = 0
+    complexity = 0
+    complexity_total = 0
+
+    for line in lines:
+        match line_type(line.coverage):
+            case LineType.hit:
+                hits += 1
+            case LineType.miss:
+                misses += 1
+            case LineType.partial:
+                partials += 1
+
+        if line.type == "b":
+            branches += 1
+        elif line.type == "m":
+            methods += 1
+
+        if line.messages:
+            messages += len(line.messages)
+
+        if isinstance(line.complexity, int):
+            complexity += line.complexity
+        elif line.complexity:
+            complexity += line.complexity[0]
+            complexity_total += line.complexity[1]
+
+    total_lines = hits + misses + partials
+
+    return ReportTotals(
+        files=0,
+        lines=total_lines,
+        hits=hits,
+        misses=misses,
+        partials=partials,
+        coverage=ratio(hits, total_lines) if total_lines else None,
+        branches=branches,
+        methods=methods,
+        messages=messages,
+        sessions=0,
+        complexity=complexity,
+        complexity_total=complexity_total,
+    )
