47fa7fd405cd4a37ab6df9f200d6b600795f2786
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2025-02-24 09:24:46 -0500

    Revert "fix: archive service bug in sentry (#521)" (#529)
    This reverts commit 835e627c4e0d951a0c980569f9dbffaf263afa41.

--- shared/api_archive/storage.py
+++ shared/api_archive/storage.py
@@ -2,53 +2,51 @@ import logging
 from datetime import timedelta
 
 from shared.config import get_config
-from shared.rollouts.features import USE_NEW_MINIO
 from shared.storage.minio import MinioStorageService
-from shared.storage.new_minio import NewMinioStorageService
 
 log = logging.getLogger(__name__)
 
 
-MINIO_SERVICE = None
+MINIO_CLIENT = None
 
 
 # Service class for interfacing with codecov's underlying storage layer, minio
-class StorageService:
-    def __init__(self, repoid: int | None = None, in_config=None):
-        global MINIO_SERVICE  # noqa: PLW0603
-
-        minio_config: dict
+class StorageService(MinioStorageService):
+    def __init__(self, in_config=None):
+        global MINIO_CLIENT  # noqa: PLW0603
 
         # init minio
         if in_config is None:
-            minio_config = get_config("services", "minio", default={})
+            self.minio_config = get_config("services", "minio", default={})
         else:
-            minio_config = in_config
+            self.minio_config = in_config
 
-        if "host" not in minio_config:
-            minio_config["host"] = "minio"
-        if "port" not in minio_config:
-            minio_config["port"] = 9000
-        if "iam_auth" not in minio_config:
-            minio_config["iam_auth"] = False
-        if "iam_endpoint" not in minio_config:
-            minio_config["iam_endpoint"] = None
+        if "host" not in self.minio_config:
+            self.minio_config["host"] = "minio"
+        if "port" not in self.minio_config:
+            self.minio_config["port"] = 9000
+        if "iam_auth" not in self.minio_config:
+            self.minio_config["iam_auth"] = False
+        if "iam_endpoint" not in self.minio_config:
+            self.minio_config["iam_endpoint"] = None
 
-        if not MINIO_SERVICE:
-            use_new = repoid and USE_NEW_MINIO.check_value(repoid)
-            if use_new:
-                MINIO_SERVICE = NewMinioStorageService(minio_config)
-            else:
-                MINIO_SERVICE = MinioStorageService(minio_config)
+        if not MINIO_CLIENT:
+            MINIO_CLIENT = self.init_minio_client(
+                self.minio_config["host"],
+                self.minio_config["port"],
+                self.minio_config["access_key_id"],
+                self.minio_config["secret_access_key"],
+                self.minio_config["verify_ssl"],
+                self.minio_config["iam_auth"],
+                self.minio_config["iam_endpoint"],
+            )
             log.info("----- created minio_client: ---- ")
-        self.minio_service = MINIO_SERVICE
+        self.minio_client = MINIO_CLIENT
 
     def create_presigned_put(self, bucket, path, expires):
-        assert MINIO_SERVICE is not None
         expires = timedelta(seconds=expires)
-        return MINIO_SERVICE.minio_client.presigned_put_object(bucket, path, expires)
+        return self.minio_client.presigned_put_object(bucket, path, expires)
 
     def create_presigned_get(self, bucket, path, expires):
-        assert MINIO_SERVICE is not None
         expires = timedelta(seconds=expires)
-        return MINIO_SERVICE.minio_client.presigned_get_object(bucket, path, expires)
+        return self.minio_client.presigned_get_object(bucket, path, expires)
