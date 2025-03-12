9ea3fc91ab8daf932a64cd1c1e621229abcf7ec0
Arpad Borsos <arpad.borsos@sentry.io>
2025-03-12 13:20:46 +0100

    Clean up Storage code
    This removes the old storage abstractions including all their tests.
    
    And also inlines the minio adapter read/write implementation back into the base class.

--- tests/unit/storage/test_memory.py
+++ tests/unit/storage/test_memory.py
@@ -1,84 +1,100 @@
 import tempfile
+from uuid import uuid4
 
 import pytest
 
 from shared.storage.exceptions import BucketAlreadyExistsError, FileNotInStorageError
 from shared.storage.memory import MemoryStorageService
-from tests.base import BaseTestCase
 
-minio_config = {
-    "access_key_id": "codecov-default-key",
-    "secret_access_key": "codecov-default-secret",
-    "verify_ssl": False,
-    "host": "minio",
-    "port": "9000",
-}
+BUCKET_NAME = "archivetest"
 
 
-class TestMemoryStorageService(BaseTestCase):
-    def test_create_bucket(self, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        bucket_name = "thiagoarchivetest"
-        res = storage.create_root_storage(bucket_name, region="")
-        assert res == {"name": "thiagoarchivetest"}
+def make_storage() -> MemoryStorageService:
+    return MemoryStorageService({})
 
-    def test_create_bucket_already_exists(self, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        bucket_name = "alreadyexists"
-        storage.root_storage_created = True
-        with pytest.raises(BucketAlreadyExistsError):
-            storage.create_root_storage(bucket_name)
 
-    def test_write_then_read_file(self, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        path = "test_write_then_read_file/result"
-        data = "lorem ipsum dolor test_write_then_read_file á"
-        bucket_name = "thiagoarchivetest"
-        writing_result = storage.write_file(bucket_name, path, data)
-        assert writing_result
-        reading_result = storage.read_file(bucket_name, path)
-        assert reading_result.decode() == data
+def ensure_bucket(storage: MemoryStorageService):
+    pass
 
-    def test_write_then_read_file_obj(self, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        path = "test_write_then_read_file/result"
-        data = "lorem ipsum dolor test_write_then_read_file á"
-        _, local_path = tempfile.mkstemp()
-        with open(local_path, "w") as f:
-            f.write(data)
-        f = open(local_path, "rb")
-        bucket_name = "archivetest"
-        writing_result = storage.write_file(bucket_name, path, f)
-        assert writing_result
 
-        _, local_path = tempfile.mkstemp()
-        with open(local_path, "wb") as f:
-            storage.read_file(bucket_name, path, file_obj=f)
-        with open(local_path, "rb") as f:
-            assert f.read().decode() == data
+def test_create_bucket():
+    storage = make_storage()
+    bucket_name = uuid4().hex
 
-    def test_read_file_does_not_exist(self, request, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        path = f"{request.node.name}/does_not_exist.txt"
-        bucket_name = "thiagoarchivetest"
-        with pytest.raises(FileNotInStorageError):
-            storage.read_file(bucket_name, path)
+    res = storage.create_root_storage(bucket_name, region="")
+    assert res == {"name": bucket_name}
 
-    def test_write_then_delete_file(self, request, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        path = f"{request.node.name}/result.txt"
-        data = "lorem ipsum dolor test_write_then_read_file á"
-        bucket_name = "thiagoarchivetest"
-        writing_result = storage.write_file(bucket_name, path, data)
-        assert writing_result
-        deletion_result = storage.delete_file(bucket_name, path)
-        assert deletion_result is True
-        with pytest.raises(FileNotInStorageError):
-            storage.read_file(bucket_name, path)
 
-    def test_delete_file_doesnt_exist(self, request, codecov_vcr):
-        storage = MemoryStorageService(minio_config)
-        path = f"{request.node.name}/result.txt"
-        bucket_name = "thiagoarchivetest"
-        with pytest.raises(FileNotInStorageError):
-            storage.delete_file(bucket_name, path)
+def test_create_bucket_already_exists():
+    storage = make_storage()
+    bucket_name = uuid4().hex
+
+    storage.create_root_storage(bucket_name)
+    with pytest.raises(BucketAlreadyExistsError):
+        storage.create_root_storage(bucket_name)
+
+
+def test_write_then_read_file():
+    storage = make_storage()
+    path = f"test_write_then_read_file/{uuid4().hex}"
+    data = "lorem ipsum dolor test_write_then_read_file á"
+
+    ensure_bucket(storage)
+    writing_result = storage.write_file(BUCKET_NAME, path, data)
+    assert writing_result
+    reading_result = storage.read_file(BUCKET_NAME, path)
+    assert reading_result.decode() == data
+
+
+def test_write_then_read_file_obj():
+    storage = make_storage()
+    path = f"test_write_then_read_file_obj/{uuid4().hex}"
+    data = "lorem ipsum dolor test_write_then_read_file_obj á"
+
+    ensure_bucket(storage)
+
+    _, local_path = tempfile.mkstemp()
+    with open(local_path, "w") as f:
+        f.write(data)
+    with open(local_path, "rb") as f:
+        writing_result = storage.write_file(BUCKET_NAME, path, f)
+    assert writing_result
+
+    _, local_path = tempfile.mkstemp()
+    with open(local_path, "wb") as f:
+        storage.read_file(BUCKET_NAME, path, file_obj=f)
+    with open(local_path, "rb") as f:
+        assert f.read().decode() == data
+
+
+def test_read_file_does_not_exist():
+    storage = make_storage()
+    path = f"test_read_file_does_not_exist/{uuid4().hex}"
+
+    ensure_bucket(storage)
+    with pytest.raises(FileNotInStorageError):
+        storage.read_file(BUCKET_NAME, path)
+
+
+def test_write_then_delete_file():
+    storage = make_storage()
+    path = f"test_write_then_delete_file/{uuid4().hex}"
+    data = "lorem ipsum dolor test_write_then_delete_file á"
+
+    ensure_bucket(storage)
+    writing_result = storage.write_file(BUCKET_NAME, path, data)
+    assert writing_result
+
+    deletion_result = storage.delete_file(BUCKET_NAME, path)
+    assert deletion_result is True
+    with pytest.raises(FileNotInStorageError):
+        storage.read_file(BUCKET_NAME, path)
+
+
+def test_delete_file_doesnt_exist():
+    storage = make_storage()
+    path = f"test_delete_file_doesnt_exist/{uuid4().hex}"
+
+    ensure_bucket(storage)
+    with pytest.raises(FileNotInStorageError):
+        storage.delete_file(BUCKET_NAME, path)
