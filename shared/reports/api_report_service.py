8f83936912606390775183987c16c1e5d5160561
Arpad Borsos <arpad.borsos@sentry.io>
2025-03-19 14:54:43 +0100

    Revert "Report caching", and remove `build_report` fn
    I don’t think we have ever turned on this feature flag, as I think the Report class is not pickle-able. And either way, deserializing from `pickle` would be slower than loading it directly from storage, most likely.
    
    For that reason, and to keep the code as simple as possible, revert this.
    Along with removing the `build_report` indirection fn which only served the purpose of defaulting the `report_class`

--- shared/reports/api_report_service.py
+++ shared/reports/api_report_service.py
@@ -1,14 +1,9 @@
 import logging
-import pickle
-import sys
 
 import sentry_sdk
-from django.core.cache import cache
 from django.utils.functional import cached_property
-from prometheus_client import Gauge
 
 from shared.api_archive.archive import ArchiveService
-from shared.config import get_config
 from shared.django_apps.core.models import Commit
 from shared.helpers.flag import Flag
 from shared.reports.readonly import ReadOnlyReport as SharedReadOnlyReport
@@ -16,10 +11,6 @@ from shared.reports.resources import Report
 from shared.storage.exceptions import FileNotInStorageError
 
 log = logging.getLogger(__name__)
-
-report_size_gauge = Gauge(
-    "codecov_report_size_bytes", "Size of the report in bytes", ["report_type"]
-)
 
 
 class ReportMixin:
@@ -50,82 +41,29 @@ class ReadOnlyReport(ReportMixin, SharedReadOnlyReport):
 
 
 @sentry_sdk.trace
-def build_report(chunks, files, sessions, totals, report_class=None):
-    if report_class is None:
-        report_class = SerializableReport
-    return report_class.from_chunks(
-        chunks=chunks, files=files, sessions=sessions, totals=totals
-    )
-
-
-@sentry_sdk.trace
 def build_report_from_commit(commit: Commit, report_class=None):
     """
     Builds a `shared.reports.resources.Report` from a given commit.
     """
+
     if not commit.report:
         return None
-
-    cache_enabled = get_config(
-        "setup", "report_service", "cache_enabled", default=False
-    )
-    archive_service = ArchiveService(commit.repository)
-
-    if cache_enabled:
-        cache_key = f"reports/cache/{commit.commitid}/{report_class.__name__ if report_class else 'SerializableReport'}"
-
-        redis_key = f"report:{commit.repository_id}:{commit.commitid}"
-        cached_report = cache.get(redis_key)
-        if cached_report:
-            # if ttl is not expired pull from archive
-            try:
-                cached_data = archive_service.read_file(cache_key)
-                if cached_data:
-                    report = pickle.loads(cached_data)
-                    return report
-            except FileNotInStorageError:
-                pass
 
     files = commit.report["files"]
     sessions = commit.report["sessions"]
     totals = commit.totals
 
     try:
-        chunks = archive_service.read_chunks(commit.commitid)
-        report = build_report(
-            chunks, files, sessions, totals, report_class=report_class
-        )
-        # Record size metrics
-        report_size = sys.getsizeof(report)
-        report_size_gauge.labels(report_type=report.__class__.__name__).set(report_size)
-        if report and cache_enabled:
-            serialized_report = pickle.dumps(report)
-
-            try:
-                archive_service.write_file(cache_key, serialized_report)
-                # Set TTL for cache in redis
-                cache.set(
-                    redis_key,
-                    "cached",
-                    timeout=get_config(
-                        "setup", "report_service", "cache_timeout", default=3600
-                    ),
-                )
-            except Exception as e:
-                log.warning(
-                    "Failed to cache report in archive",
-                    extra=dict(
-                        commit=commit.commitid, repo=commit.repository_id, error=str(e)
-                    ),
-                )
-
-        return report
+        chunks = ArchiveService(commit.repository).read_chunks(commit.commitid)
     except FileNotInStorageError:
         log.warning(
             "File for chunks not found in storage",
-            extra=dict(
-                commit=commit.commitid,
-                repo=commit.repository_id,
-            ),
+            extra=dict(commit=commit.commitid, repo=commit.repository_id),
         )
         return None
+
+    if report_class is None:
+        report_class = SerializableReport
+    return report_class.from_chunks(
+        chunks=chunks, files=files, sessions=sessions, totals=totals
+    )
