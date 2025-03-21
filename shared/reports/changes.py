69b8fa793fa33fb4d3c0e3de9c5d7abaf57434a0
Arpad Borsos <arpad.borsos@sentry.io>
2025-03-21 15:18:09 +0100

    Remove the `get_changes_using_rust` fn (#584)
    This function was called within worker in "comparison mode", that ran both rust and python implementations side by side, only to always prefer the python results, and spam the logs with differences between the two that were never resolved, and I doubt anyone has looked at those in recent times.
    
    So lets just remove this code so there is less to maintain and to worry about going forward.

--- shared/reports/changes.py
+++ shared/reports/changes.py
@@ -1,7 +1,5 @@
 import cc_rustyribs
 
-from shared.reports.types import Change
-
 
 def run_comparison_using_rust(base_report, head_report, diff):
     return cc_rustyribs.run_comparison(
@@ -9,47 +7,3 @@ def run_comparison_using_rust(base_report, head_report, diff):
         head_report.rust_report.get_report(),
         cc_rustyribs.rustify_diff(diff),
     )
-
-
-def get_changes_using_rust(base_report, head_report, diff):
-    return _get_changes_from_comparison(
-        run_comparison_using_rust(base_report, head_report, diff)
-    )
-
-
-def _get_changes_from_comparison(data):
-    changes = []
-    for found_change in data["files"]:
-        if found_change["unexpected_line_changes"]:
-            # temporary logic just to ensure we know all differences between
-            # python changes and rust changes
-            # on python, two partial lines are not considered unexpected changes
-            # even if the fractions inside are different
-            # in rust they are considered different, so we might see a few:
-            # "unexpected_line_changes": [[[1, "p"], [1, "p"]]],
-            # on rust
-            has_actual_expected_line_changes = any(
-                b[1] != h[1] for (b, h) in found_change["unexpected_line_changes"]
-            )
-            if has_actual_expected_line_changes:
-                changes.append(
-                    Change(
-                        path=found_change["head_name"],
-                        in_diff=bool(found_change["added_diff_coverage"]),
-                        old_path=found_change.get("base_name")
-                        if found_change["base_name"] != found_change["head_name"]
-                        else None,
-                        totals=None,
-                        new=(
-                            found_change["head_coverage"] is not None
-                            and found_change["base_coverage"] is None
-                            and not found_change["file_was_added_by_diff"]
-                        ),
-                        deleted=(
-                            found_change["base_coverage"] is not None
-                            and found_change["head_coverage"] is None
-                            and not found_change["file_was_removed_by_diff"]
-                        ),
-                    )
-                )
-    return changes
