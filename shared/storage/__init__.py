a211c6fc861aebc16baeb3b006ca99263c658e7b
Joseph Sawaya <joseph.sawaya@sentry.io>
2025-03-25 12:55:09 -0400

    feat: add metadata to MinioStorageService
    GCS/S3 objects allow you to add metadata to them when writing,
    and you can read that metadata back. This is done via request/response
    headers. The reason I chose to pass in a metadata_container instead of
    changing the return type of read_file is because I wanted to offer
    the option to not care about an object's metadata, and this also makes
    it so this PR won't involve a bunch of changes in worker and API to
    accept 2 args as the return instead of 1.

--- shared/storage/__init__.py
+++ shared/storage/__init__.py
@@ -1,8 +1,7 @@
 from shared.config import get_config
-from shared.storage.base import BaseStorageService
 from shared.storage.minio import MinioStorageService
 
 
-def get_appropriate_storage_service(*_args, **_kwargs) -> BaseStorageService:
+def get_appropriate_storage_service(*_args, **_kwargs) -> MinioStorageService:
     minio_config = get_config("services", "minio", default={})
     return MinioStorageService(minio_config)
